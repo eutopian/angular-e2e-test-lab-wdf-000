07bc51a289a19bc081432e3b6b4109dffb1ed1e8
Margaret <eutopia@gmail.com>
2016-11-29 14:20:07 -0500

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,7 +1,13 @@
 var path = require('path');
 
-describe('angularjs homepage todo list', function() {
+describe('angularjs add contact', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+		element(by.css('[value="add"]')).click();
+		var contacts = element.all(by.repeater('contact in vm.contacts'));
+		expect(contacts.count()).toEqual(4);
+		var tim = contacts.get(1);
+		expect(tim.element(by.model('contact.name')).getAttribute('value')).toBe('Tim');
+		expect(tim.element(by.model('contact.phone')).getAttribute('value')).toBe('3934203242');
 	});
 });
